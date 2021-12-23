f8b911da1d1e3c0db7a569518d3b91c674ff5e17
kratu92 <kratux92@gmail.com>
2021-12-24 00:10:30 +0100

    Replaced mysql-create-db-and-user-script with function. Improved queries

--- mysql-create-db-and-user.sh
+++ mysql-create-db-and-user.sh
@@ -3,86 +3,96 @@
 # Initial config
 . ./inc/init-script.sh
 
-printf "${BLUE} ╔═══════════════════════════════════════════════╗${NC}\n"
-printf "${BLUE} ║ -> KRATU'S MYSQL DB AND USER CREATE SCRIPT <- ║${NC}\n"
-printf "${BLUE} ╚═══════════════════════════════════════════════╝${NC}\n"
+############################################
+# mySQL Create Database and User
+############################################
+mySQLCreateDbAndUser() {
 
-# Ask for confirmation
-confirm -e
+	printf "${BLUE} ╔═══════════════════════════════════════════════╗${NC}\n"
+	printf "${BLUE} ║ -> KRATU'S MYSQL DB AND USER CREATE SCRIPT <- ║${NC}\n"
+	printf "${BLUE} ╚═══════════════════════════════════════════════╝${NC}\n"
 
-# Previous checks
-previousChecks -rv
+	# Ask for confirmation
+	confirm -e
 
-# Make sure mysql is installed
-! test -x /usr/sbin/mysqld \
-        && printf "${RED}Error: nginx is not installed${NC}\n" \
-        && exit 1
+	# Previous checks
+	previousChecks -rv
 
-# Check arguments
-if [ -z "$1" ]; then
-	db=""
-	user=""
-	pass=""
-else
-	if [ "$#" -ne 3 ]; then 
-		printf "${RED}Error: Invalid number of arguments supplied${NC}\n"
-		printf "${YELLOW}Number of arguments allowed: 0 or 3 (db, user, password)${NC}\n"
-		exit 1
-	fi
+	# Make sure mysql is installed
+	! test -x /usr/sbin/mysqld \
+        	&& printf "${RED}Error: nginx is not installed${NC}\n" \
+	        && exit 1
 
-	db=$1
-	user=$2
-	pass=$3
-fi
-
-# Ask for a DB name
-until [ "${db}" != "" ]
-do
-        read -p "Set a name for the new database: " db
-        db="$(echo -e "${db,,}" | sed -e 's/^[[:space:]]*//')";
-done
-
-# Ask for a username
-until [ "${user}" != "" ]
-do
-        read -p "Set a username: " user
-        user="$(echo -e "${user,,}" | sed -e 's/^[[:space:]]*//')";
-done
-
-# Ask for password
-until [ "${pass}" != "" ]
-do
-        read -s -p "Set a password for the user ${user}: " pass
-        pass="$(echo -e "${pass,,}" | sed -e 's/^[[:space:]]*//')";
-	echo "" # New line
-
-	if [ "${pass}" != "" ]
-		then read -s -p "Retype the password for user ${user}: " passConfirm
-		passConfirm="$(echo -e "${passConfirm,,}" | sed -e 's/^[[:space:]]*//')";
-		echo "" # New line 
-
-		if [ "${pass}" != "${passConfirm}" ]
-			then printf "${RED}Password does not match. Try setting the password again.${NC}\n"
-			pass=""
+	# Check arguments
+	if [ -z "$1" ]; then
+		db=""
+		user=""
+		pass=""
+	else
+		if [ "$#" -ne 3 ]; then 
+			printf "${RED}Error: Invalid number of arguments supplied${NC}\n"
+			printf "${YELLOW}Number of arguments allowed: 0 or 3 (db, user, password)${NC}\n"
+			exit 1
 		fi
+
+		db=$1
+		user=$2
+		pass=$3
 	fi
-done
 
-# Confirm selected data
-echo "The new database will be created with the following data:"
-printf "${WHITE}- database name: ${PURPLE}${db}${NC}\n"
-printf "${WHITE}- username: ${PURPLE}${user}${NC}\n"
+	# Ask for a DB name
+	until [ "${db}" != "" ]
+	do
+        	read -p "Set a name for the new database: " db
+	        db="$(echo -e "${db,,}" | sed -e 's/^[[:space:]]*//')";
+	done
 
-confirm -e
+	# Ask for a username
+	until [ "${user}" != "" ]
+	do
+        	read -p "Set a username: " user
+	        user="$(echo -e "${user,,}" | sed -e 's/^[[:space:]]*//')";
+	done
 
-query1="CREATE DATABASE IF NOT EXISTS ${db};"
-query2="CREATE USER IF NOT EXISTS '${user}'@'%' IDENTIFIED BY '${pass}';"
-query3="GRANT ALL PRIVILEGES ON ${db}.* TO '${user}'@'%';"
-query4="FLUSH PRIVILEGES;"
+	# Ask for password
+	until [ "${pass}" != "" ]
+	do
+        	read -s -p "Set a password for the user ${user}: " pass
+	        pass="$(echo -e "${pass,,}" | sed -e 's/^[[:space:]]*//')";
+		echo "" # New line
 
-sql="${query1} ${query2} ${query3} ${query4}"
+		if [ "${pass}" != "" ]
+			then read -s -p "Retype the password for user ${user}: " passConfirm
+			passConfirm="$(echo -e "${passConfirm,,}" | sed -e 's/^[[:space:]]*//')";
+			echo "" # New line 
 
-printf "${GREEN}You will be asked to introduce MySQL's root password${NC}\n"
-`command -v mysql` -u root -p -e "${sql}"
+			if [ "${pass}" != "${passConfirm}" ]
+				then printf "${RED}Password does not match. Try setting the password again.${NC}\n"
+				pass=""
+			fi
+		fi
+	done
 
-printf "${GREEN}MySQL user and database where created${NC}\n"
+	# Confirm selected data
+	echo "The new database will be created with the following data:"
+	printf "${WHITE}- database name: ${PURPLE}${db}${NC}\n"
+	printf "${WHITE}- username: ${PURPLE}${user}${NC}\n"
+
+	confirm -e
+
+	query1="CREATE DATABASE IF NOT EXISTS \`${db}\`;"
+	query2="CREATE USER IF NOT EXISTS '${user}'@'%' IDENTIFIED BY '${pass}';"
+	query3="GRANT ALL PRIVILEGES ON \`${db}\`.* TO '${user}'@'%';"
+	query4="FLUSH PRIVILEGES;"
+	query5="SELECT COUNT(DISTINCT(user)) AS 'Current users' FROM mysql.db;"
+        query6="SELECT db, user, host FROM mysql.db; SHOW DATABASES;"
+
+	sql="${query1} ${query2} ${query3} ${query4} ${query5} ${query6}"
+
+	printf "${GREEN}You will be asked to introduce MySQL's root password${NC}\n"
+	`command -v mysql` -u root -p -e "${sql}"
+
+	printf "${GREEN}MySQL user [${PURPLE}${user}${GREEN}] and database [${PURPLE}${db}${GREEN}] where created${NC}\n"
+}
+
+mySQLCreateDbAndUser
